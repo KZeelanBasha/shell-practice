7d4ffd2b0c404f7b3e9d0f62df2dd1df5d3ea325
KZeelanBasha <kzeelanbasha@gmail.com>
2025-05-16 17:15:10 +0530

    code extended

---
+++ 03-special-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "all variables passed to the script: $@"
+echo "number of variables: $#"
+echo "script name: $0"
+echo "current directory: $PWD"
+echo "user is running this scripr: $USER"
+echo "home directory of user: $HOME"
+echo "PID of the script: $$"

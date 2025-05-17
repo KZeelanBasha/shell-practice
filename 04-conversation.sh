104ead0b670cc3e19eb27d30d63890bdc7b4d0e1
KZeelanBasha <kzeelanbasha@gmail.com>
2025-05-17 09:48:20 +0530

    new file created

---
+++ 04-conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "trump:: hey musk ..how are you ?"
+echo "musk:: i am fine ....what about you."
+echo "trump:: whats up?"
+echo "musk:: nothing !..just going to mars........will you come ?"
+echo "trump:: ohh !! thats great.....you carry on....i will come once you come back.....happy journey !"

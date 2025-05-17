beb5438168b65e238b729ca0b328e72310a9771b
KZeelanBasha <kzeelanbasha@gmail.com>
2025-05-17 11:00:13 +0530

    changed

---
+++ 09-array.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+movie=("kalaigar" "zeelan" "basha")
+
+echo "movie name is:: ${movie[0]}"
+echo "movie name is:: ${movie[1]}"
+echo "movie name is:: ${movie[2]}"
+echo "all movies are:: ${movie[@]}"

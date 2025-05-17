9b029c1381f572657cbca9b7387685bbd5e17b0d
KZeelanBasha <kzeelanbasha@gmail.com>
2025-05-17 10:06:49 +0530

    changed

--- 05.variables.sh
+++ 05.variables.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-PERSON1=trump
+PERSON1=modi
 PERSON2=musk
 echo "$PERSON1:: hey $PERSON2 ..how are you ?"
 echo "$PERSON2:: i am fine ....what about you."
-echo "$PERSON1:: whats up?"
+echo "$PERSON1:: i am fine too.....whats up?"
 echo "$PERSON2:: nothing !..just going to mars........will you come ?"
 echo "$PERSON1:: ohh !! thats great.....you carry on....i will come once you come back.....happy journey !"

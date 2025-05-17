bdcb6bbb7cf8515d3f0b5eae0aa51aa4cdcaaf5d
KZeelanBasha <kzeelanbasha@gmail.com>
2025-05-17 10:20:25 +0530

    created arguments file

---
+++ 06-arguments.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2
+echo "$PERSON1:: hey $PERSON2 ..how are you ?"
+echo "$PERSON2:: i am fine ....what about you."
+echo "$PERSON1:: i am fine too.....whats up?"
+echo "$PERSON2:: nothing !..just going to mars........will you come ?"
+echo "$PERSON1:: ohh !! thats great.....you carry on....i will come once you come back.....happy journey !"
